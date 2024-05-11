36cc49bf73a63bc1f6d6faf0c625ee646dab1bb3
manttoni <48520763+manttoni@users.noreply.github.com>
2024-05-11 14:59:05 +0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (*needle == '\0')
 		return ((char *) haystack);
 	i = 0;
-	while (haystack[i])
+	while (haystack[i] && i < len)
 	{
 		j = 0;
 		while (haystack[i + j] == needle[j] && i + j < len)
